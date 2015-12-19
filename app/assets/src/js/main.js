07db5e52df4e9031eaa155eba57b4d8881d61400
sugyan <sugi1982@gmail.com>
2015-12-20 07:42:12 +0900

    use tagsinput

--- app/assets/src/js/main.js
+++ app/assets/src/js/main.js
@@ -1,3 +1,6 @@
-$(() => {
-    $.getScript('//platform.twitter.com/widgets.js');
+$(document).on('ready page:change', () => {
+    if (window.location.pathname.match('^/labels/[0-9]+/')) {
+        const label_tag  = $('input[name="label[tags]"]');
+        label_tag.tagsinput();
+    }
 });

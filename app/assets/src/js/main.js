8e6e675f5b008d52fda42ee90abe5efaa7fc7dfd
sugyan <sugi1982@gmail.com>
2015-12-21 20:58:04 +0900

    add typeahead.css
    copied from http://twitter.github.io/typeahead.js/css/examples.css

--- app/assets/src/js/main.js
+++ app/assets/src/js/main.js
@@ -1,7 +1,24 @@
-/* global $ */
-$(document).on('ready page:change', () => {
-    if (window.location.pathname.match('^/labels/[0-9]+/')) {
-        const label_tag  = $('input[name="label[tags]"]');
-        label_tag.tagsinput();
+/* global $, Bloodhound */
+$(document).on('ready page:load', () => {
+    if (window.location.pathname.match('^/labels/')) {
+        let input = $('input[name="label[tags]"]');
+        input.tagsinput();
+    }
+    if (window.location.pathname.match('^/faces/')) {
+        let input = $('input[name="face[label_id]"]');
+        let source = new Bloodhound({
+            initialize: false,
+            local: ['dog', 'pig', 'moose'],
+            queryTokenizer: Bloodhound.tokenizers.whitespace,
+            datumTokenizer: Bloodhound.tokenizers.whitespace
+        });
+        source.initialize();
+        input.typeahead({
+            minLength: 1,
+            highlight: true
+        }, {
+            source: source
+        });
+        input.focus();
     }
 });

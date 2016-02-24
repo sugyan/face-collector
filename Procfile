676432aeb03511d97d65ce8add06c44a367e6c9f
sugyan <sugi1982@gmail.com>
2016-02-24 23:21:47 +0900

    update for clockwork jobs

--- Procfile
+++ Procfile
@@ -1,2 +1,2 @@
 web: bundle exec puma -C config/puma.rb
-worker: bundle exec clockwork etc/clock.rb
+clock: bundle exec clockwork etc/clock.rb
--- etc/clock.rb
+++ etc/clock.rb
@@ -9,6 +9,13 @@ module Clockwork
   end
 
   every(1.hour, 'collect photos') do
-    Rake::Task
+    Rake::Task['collect_photos:twitter'].invoke
+  end
+
+  every(1.day, 'db backup', at: '00:00') do
+    database = Rails.configuration.database_configuration[Rails.env]['database']
+    dest = File.join(Rails.root, 'var', 'backups', format('backup-%s.dump', Time.zone.today.to_s))
+    cmd = format('pg_dump -Fc --no-acl --no-owner -h localhost %s > %s', database, dest)
+    puts 'backup succeeded' if system(cmd)
   end
 end

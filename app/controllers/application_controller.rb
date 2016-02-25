95c73161bc0ced125b7d20e4cc92f482dacae9ec
sugyan <sugi1982@gmail.com>
2016-02-25 23:37:06 +0900

    default_url_options

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,14 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  def default_url_options
+    if Rails.env.production?
+      super.merge(host: ENV['DOMAIN_NAME'], port: nil)
+    else
+      {}
+    end
+  end
+
   def after_sign_in_path_for(_)
     collector_path
   end

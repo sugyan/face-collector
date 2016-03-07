1e8da74542566ae6ebd4115c17f8ce8ec535cb7c
sugyan <sugi1982@gmail.com>
2016-03-08 00:44:04 +0900

    fix controller

--- app/controllers/queries_controller.rb
+++ app/controllers/queries_controller.rb
@@ -28,7 +28,7 @@ class QueriesController < ApplicationController
 
     respond_to do |format|
       if @query.save
-        format.html { redirect_to collector_query_url(@query), notice: 'Query was successfully created.' }
+        format.html { redirect_to query_url(@query), notice: 'Query was successfully created.' }
         format.json { render :show, status: :created, location: @query }
       else
         format.html { render :new }
@@ -42,7 +42,7 @@ class QueriesController < ApplicationController
   def update
     respond_to do |format|
       if @query.update(query_params)
-        format.html { redirect_to collector_query_url(@query), notice: 'Query was successfully updated.' }
+        format.html { redirect_to query_url(@query), notice: 'Query was successfully updated.' }
         format.json { render :show, status: :ok, location: @query }
       else
         format.html { render :edit }
@@ -56,7 +56,7 @@ class QueriesController < ApplicationController
   def destroy
     @query.destroy
     respond_to do |format|
-      format.html { redirect_to collector_queries_url, notice: 'Query was successfully destroyed.' }
+      format.html { redirect_to queries_url, notice: 'Query was successfully destroyed.' }
       format.json { head :no_content }
     end
   end

940af6f248850f126172fec0614535adfe427880
sugyan <sugi1982@gmail.com>
2016-03-11 01:55:19 +0900

    update devise authentication

--- config/routes.rb
+++ config/routes.rb
@@ -19,8 +19,5 @@ Rails.application.routes.draw do
   end
 
   # devise
-  devise_for :users, controllers: { omniauth_callbacks: 'users/omniauth_callbacks' }
-  devise_scope :user do
-    get 'sign_out', to: 'devise/sessions#destroy', as: :destroy_user_session
-  end
+  devise_for :users
 end

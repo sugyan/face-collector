c37a7bab27b64d71df8e0e12a5a4001623b8c7e7
sugyan <sugi1982@gmail.com>
2016-03-21 21:07:16 +0900

    fix tests

--- test/controllers/labels_controller_test.rb
+++ test/controllers/labels_controller_test.rb
@@ -1,54 +1,52 @@
 require 'test_helper'
 
-module Collector
-  class LabelsControllerTest < ActionController::TestCase
-    include Devise::TestHelpers
+class LabelsControllerTest < ActionController::TestCase
+  include Devise::TestHelpers
 
-    setup do
-      @label = labels(:one)
-      sign_in users(:one)
+  setup do
+    @label = labels(:one)
+    sign_in users(:one)
+  end
+
+  test 'should get index' do
+    get :index
+    assert_response :success
+    assert_not_nil assigns(:labels)
+  end
+
+  test 'should get new' do
+    get :new
+    assert_response :success
+  end
+
+  test 'should create label' do
+    assert_difference('Label.count') do
+      post :create, label: { name: @label.name, tags: @label.tags }
     end
 
-    test 'should get index' do
-      get :index
-      assert_response :success
-      assert_not_nil assigns(:labels)
+    assert_redirected_to label_path(assigns(:label))
+  end
+
+  test 'should show label' do
+    get :show, id: @label
+    assert_response :success
+  end
+
+  test 'should get edit' do
+    get :edit, id: @label
+    assert_response :success
+  end
+
+  test 'should update label' do
+    patch :update, id: @label, label: { name: @label.name, tags: @label.tags }
+    assert_redirected_to label_path(assigns(:label))
+  end
+
+  test 'should destroy label' do
+    assert_difference('Label.count', -1) do
+      delete :destroy, id: @label
     end
 
-    test 'should get new' do
-      get :new
-      assert_response :success
-    end
-
-    test 'should create label' do
-      assert_difference('Label.count') do
-        post :create, label: { name: @label.name, tags: @label.tags }
-      end
-
-      assert_redirected_to collector_label_path(assigns(:label))
-    end
-
-    test 'should show label' do
-      get :show, id: @label
-      assert_response :success
-    end
-
-    test 'should get edit' do
-      get :edit, id: @label
-      assert_response :success
-    end
-
-    test 'should update label' do
-      patch :update, id: @label, label: { name: @label.name, tags: @label.tags }
-      assert_redirected_to collector_label_path(assigns(:label))
-    end
-
-    test 'should destroy label' do
-      assert_difference('Label.count', -1) do
-        delete :destroy, id: @label
-      end
-
-      assert_redirected_to collector_labels_path
-    end
+    assert_redirected_to labels_path
   end
 end

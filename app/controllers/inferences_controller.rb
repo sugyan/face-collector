c2ff701a90235b77f9581357c7215665a374276e
sugyan <sugi1982@gmail.com>
2016-10-08 02:07:02 +0900

    use simple_token_authentication

--- app/controllers/inferences_controller.rb
+++ app/controllers/inferences_controller.rb
@@ -1,4 +1,5 @@
 class InferencesController < ApplicationController
+  acts_as_token_authentication_handler_for User
   before_action :authenticate_user!
 
   def index
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ApplicationRecord
+  acts_as_token_authenticatable
   devise :database_authenticatable, :rememberable, :trackable
 end

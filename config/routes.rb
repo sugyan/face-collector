6ac883d9872296afc45dc47fdbdbd5e83ad61996
sugyan <sugi1982@gmail.com>
2016-10-27 20:23:19 +0900

    update routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,16 +2,10 @@ Rails.application.routes.draw do
   root 'root#index'
   get '/root' => 'root#index'
   get '/feed' => 'root#feed'
+end
 
-  resources :queries
-  resources :labels do
-    member do
-      get 'faces'
-      get 'faces_list'
-      get 'inferences'
-    end
-    get 'all', on: :collection
-  end
+# faces
+Rails.application.routes.draw do
   resources :faces, only: [:index, :show, :destroy] do
     member do
       get 'image'
@@ -25,21 +19,43 @@ Rails.application.routes.draw do
       get 'tfrecords/:index_number' => 'faces#tfrecords'
     end
   end
+end
+
+# labels
+Rails.application.routes.draw do
+  resources :labels do
+    member do
+      get 'faces'
+      get 'faces_list'
+      get 'inferences'
+    end
+    get 'all', on: :collection
+  end
+end
+
+# others
+Rails.application.routes.draw do
+  resources :queries
+  resources :feedbacks, only: [:index, :create]
+
   resources :inferences, only: [:index] do
     member do
       post 'accept'
     end
   end
-  resources :feedbacks, only: [:index, :create]
+end
 
-  # devise
-  devise_for :users
-  resources :users, only: [:index, :show]
-
-  # recognizer
+# recognizer
+Rails.application.routes.draw do
   namespace :recognizer do
     root 'root#index', as: :root
     get  'about' => 'root#about'
     post 'api'   => 'root#api'
   end
+end
+
+# devise, users
+Rails.application.routes.draw do
+  devise_for :users
+  resources :users, only: [:index, :show]
 end

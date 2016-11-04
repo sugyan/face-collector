46340076fa86e5867c17a4b70188beea0f0c890c
sugyan <sugi1982@gmail.com>
2016-11-04 18:25:33 +0900

    handle ActiveRecord::RecordNotFound exception

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,11 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+  rescue_from ActiveRecord::RecordNotFound do |exception|
+    logger.warn(exception)
+    render file: Rails.root.join('public/404.html'), status: 404, layout: false
+  end
+
   def after_sign_in_path_for(_)
     root_path
   end
